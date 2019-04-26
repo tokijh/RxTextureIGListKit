2ece02749b6128d13f05e1ce2e165961ca927b15
tokijh <tokijh@naver.com>
2019-04-26 10:16:18 +0900

    Add dropped file

---
+++ RxTextureIGListKit/Source/Common/Pure/Payload/PayloadConfiguratorModule.swift
@@ -0,0 +1,38 @@
+//
+//  PayloadConfiguratorModule.swift
+//  RxTextureIGListKit
+//
+//  Created by 윤중현 on 25/04/2019.
+//  Copyright © 2019 tokijh. All rights reserved.
+//
+
+/// A module that can be configurated with an existing instance.
+public protocol PayloadConfiguratorModule: class, PayloadModule {
+
+    /// A configurator for `Self`.
+    associatedtype PayloadConfigurator = RxTextureIGListKit.PayloadConfigurator<Self>
+
+    /// Configures an existing module with a dependency and a payload.
+    func configure(dependency: Dependency, payload: Payload)
+}
+
+public extension PayloadConfiguratorModule where Dependency == Void {
+    /// Configures an existing module with a payload.
+    func configure(payload: Payload) {
+        self.configure(dependency: Void(), payload: payload)
+    }
+}
+
+public extension PayloadConfiguratorModule where Payload == Void {
+    /// Configures an existing module with a dependency.
+    func configure(dependency: Dependency) {
+        self.configure(dependency: dependency, payload: Void())
+    }
+}
+
+public extension PayloadConfiguratorModule where Dependency == Void, Payload == Void {
+    /// Configures an existing module.
+    func configure() {
+        self.configure(dependency: Void(), payload: Void())
+    }
+}
---
+++ RxTextureIGListKit/Source/Scene/Unsplash/UnsplashList/UnsplashListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UnsplashListViewController.swift
+//  RxTextureIGListKit
+//
+//  Created by 윤중현 on 25/04/2019.
+//  Copyright © 2019 tokijh. All rights reserved.
+//
+
+import UIKit
+
+final class UnsplashListViewController: BaseViewController, FactoryModule {
+
+}
